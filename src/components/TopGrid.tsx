daf1448dc627420ba57981f9e7c9dbda4b49cceb
mgabrielnt <118196810+mgabrielnt@users.noreply.github.com>
2026-07-05 16:38:09 +0700

    Apply Turret Road only to top grid

--- src/components/TopGrid.tsx
+++ src/components/TopGrid.tsx
@@ -6,7 +6,7 @@ import { RecognitionBlock } from "./RecognitionBlock";
 
 export function TopGrid() {
   return (
-    <div className="relative z-10 grid gap-6 text-[12px] md:grid-cols-2 xl:grid-cols-[1.2fr_1.2fr_1.45fr_1.25fr_1.2fr] xl:gap-10">
+    <div className="top-display-font relative z-10 grid gap-6 text-[12px] md:grid-cols-2 xl:grid-cols-[1.2fr_1.2fr_1.45fr_1.25fr_1.2fr] xl:gap-10">
       <IntroBlock />
       <PositionBlock />
       <RecognitionBlock />
